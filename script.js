ae9ffeb439141ce4bd013fda280bec5ac621a13d
bruch0 <lucas.bruch0@gmail.com>
2021-07-28 10:26:07 -0300

    made each card get a random parrot gif

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ let selected;
 let previous;
 let qty = 0;
 let div = ''
-const cards = []
+const cards = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn']
+
+let sort_cards = cards.sort(comparador)
 
 function startGame() {
     while (qty < 1 || qty > 15 || qty % 2 !== 0) {
         qty = parseInt(prompt("Com quantas cartas você quer jogar?"))
     }
 
-    for (let i = 0; i < qty; i++) {
-        div += `<button onclick="virarCarta(this)">
-                    <div class="front-face">
-                        <img src="assets/front.png" alt="Imagem de um papagaio">
-                    </div>
-                    <div class="back-face">
-                        <img src="assets/bobrossparrot.gif" alt="GIF de um papagaio">
-                    </div>
-                </button>`
+    for (let j = 0; j < 2; j++){
+        for (let i = 0; i < (qty / 2); i++) {
+            div += `<button onclick="virarCarta(this)">
+                        <div class="front-face">
+                            <img src="assets/front.png" alt="Imagem de um papagaio">
+                        </div>
+                        <div class="back-face">
+                            <img src="assets/${sort_cards[i]}parrot.gif" alt="GIF de um papagaio">
+                        </div>
+                    </button>`
+        }
     }
     document.querySelector(".container").innerHTML = div;
 }
@@ -33,5 +37,8 @@ function virarCarta(element) {
     console.log(element)
 }
 
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 startGame()
