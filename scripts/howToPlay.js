7290b6e967451357aba9e62c9a78209c8a9c11ac
Bruna Topan <brutopanb@gmail.com>
2021-08-04 10:03:34 -0300

    Preparando review

--- scripts/howToPlay.js
+++
@@ -1,9 +0,0 @@
-function helpOpenner() {
-    document.querySelector('footer').style.display = 'none';
-    document.querySelector('.help').style.display = 'initial';
-}
-
-function helpCloser() {
-    document.querySelector('footer').style.display = 'flex';
-    document.querySelector('.help').style.display = 'none';
-}
